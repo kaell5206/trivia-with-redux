28f18fde2b9b18e3d3ba309f47ce330465b7456b
Carlos Moura <carlosmourapires88@gmail.com>
2022-04-05 15:53:35 -0300

    Requisito 1 concluido

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
