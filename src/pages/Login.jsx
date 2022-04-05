6f38173c59c7f2d170690d3cd3430ea966c076ff
Carlos Moura <carlosmourapires88@gmail.com>
2022-04-05 16:33:00 -0300

    Requisito 3 concluido

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../trivia.png';
 
 class Login extends React.Component {
   constructor() {
@@ -27,39 +29,47 @@ class Login extends React.Component {
   render() {
     const { name, email, buttonDisabled } = this.state;
     return (
-      <div>
-        <label htmlFor="name">
-          Nome:
-          <input
-            name="name"
-            data-testid="input-player-name"
-            id="name"
-            type="text"
-            value={ name }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <br />
-        <label htmlFor="email">
-          E-mail:
-          <input
-            name="email"
-            data-testid="input-gravatar-email"
-            id="email"
-            type="email"
-            value={ email }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <br />
-        <button
-          data-testid="btn-play"
-          type="button"
-          disabled={ buttonDisabled }
-        >
-          Play
-        </button>
-      </div>
+
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <div>
+          <label htmlFor="name">
+            Nome:
+            <input
+              name="name"
+              data-testid="input-player-name"
+              id="name"
+              type="text"
+              value={ name }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <label htmlFor="email">
+            E-mail:
+            <input
+              name="email"
+              data-testid="input-gravatar-email"
+              id="email"
+              type="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <button
+            data-testid="btn-play"
+            type="button"
+            disabled={ buttonDisabled }
+          >
+            Play
+          </button>
+          <Link to="/settings">
+            <button data-testid="btn-settings" type="button">Configurações</button>
+          </Link>
+        </div>
+      </header>
+
     );
   }
 }
